a960f78db6da16d69cefa9deaea5df188f23b05c
roginfarrer <rogin@roginfarrer.com>
2014-12-19 22:28:31 -0500

    Organize assets

--- config.rb
+++ config.rb
@@ -3,13 +3,12 @@ require 'compass/import-once/activate'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "css"
-sass_dir = "_sass"
-images_dir = "images"
-javascripts_dir = "javascripts"
+css_dir = "/css"
+sass_dir = "_assets/scss"
+javascripts_dir = "_assets/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
+output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
